f48925b92c4d7ccfa0b4591cde92092e71b70ff0
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-10-04 17:30:15 -0300

    Tela inicial pronta

---
+++ cadastrar.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Compiled and minified CSS -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        
+
+    <title>Cinema PHP</title>
+</head>
+<body>
+<nav class="nav-extended  blue darken-1">
+    <div class="nav-wrapper">
+      <ul id="nav-mobile" class="right">
+        <li><a href="galeria.php">Galeria</a></li>
+        <li><a href="cadastrar.php">Cadastrar</a></li>
+      </ul>
+    </div>
+    <div class="nav-header center"><h1>Cinema PHP</h1></div>
+    <div class="nav-content  indigo darken-4">
+      <ul class="tabs tabs-transparent">
+        <li class="tab"><a href="#test1">Todos</a></li>
+        <li class="tab"><a class="active" href="#test2">Assistidos</a></li>
+        <li class="tab"><a href="#test3">Favoritos</a></li>
+      </ul>
+    </div>
+  </nav>
+        
+  <div class="row">
+      <div class="col s6 offset-m3 hoverable">
+          <div class="card">
+              <div class="card-content">
+                  <span class="card-title">Cadastrar Filme</span>
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+              </div>
+              <div class="card-action">
+                  <a href="#">This is a link</a>
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+</body>
+</html>
